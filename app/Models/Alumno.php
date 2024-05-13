c412779b71662353de3dc32966637282407ecc9c
alexcvsoft <alexcarrillov19@gmail.com>
2024-05-13 11:16:27 -0600

    Se añadió el modelo y migraciones de Alumno

---
+++ app/Models/Alumno.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Alumno extends User
+{
+    use HasFactory;
+
+    protected $fillable = ['semestre', 'nivel_academico_id'];
+
+    /**
+     * Obtener el Usuario asociado con el Alumno.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'usuario_id');
+    }
+
+    /**
+     * Obtener el Nivel Académico del Alumno
+     */
+    public function nivelAcademico()
+    {
+        return $this->belongsTo(NivelAcademico::class, 'nivel_academico_id');
+    }
+}
