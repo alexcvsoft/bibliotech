4ed5670b3b144d3a7862a8ba7d81c00284c11955
Yerení JR Jiménez <32132969@uaz.edu.mx>
2024-05-14 20:59:26 -0600

    Feat: Plantilla
    -Se agregan los archivos de css, assets, icomoon, images, js a la carpeta public/plantilla.
    -Se modifica la vista para al ingresar redireccione a Home de plantilla.
    -Cambio en web para añadir la ruta de redirección a la plantilla

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
+Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified',])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
 });
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::redirect('/', '/home');
+/* Route::resource('invitados', App\Http\Controllers\InvitadoController::class); */
