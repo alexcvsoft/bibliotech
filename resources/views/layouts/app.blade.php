882d7d7cbd5f42dd9de3ad292240efcae27dadfd
sonnyjmoore <le21450049@itz.edu.mx>
2024-05-20 08:44:36 -0600

    Vista 2
    PLantilla implementada, solo falta una pantalla de carga

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,13 +23,13 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="{{ asset('plantilla/css/vendor.css') }}">
-        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="stylesheet" type="text/css" href="{{ asset('plantilla/css/style.css') }}">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;900&display=swap" rel="stylesheet">
         <!-- Scripts -->
         <!-- {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}} -->
-
+ 
         <!-- Styles -->
         @livewireStyles
     </head>
@@ -242,7 +242,8 @@
         <script src="{{ asset('plantilla/js/plugins.js') }}"></script>
         <script src="{{ asset('plantilla/js/script.js') }}"></script>
         <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
+        <script src="{{ asset('plantilla/js/jquery-1.11.0.min.js') }}"></script>
     </body>
-    
+
     @yield('content')
 </html>
